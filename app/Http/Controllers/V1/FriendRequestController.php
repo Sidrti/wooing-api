7bfbe32a8771b83f7f67550b14f797347dc14801
siddhant.kumar@bizzydesk.com <siddhant.singh326@gmail.com>
2024-03-07 16:20:24 +0530

    bugs

--- app/Http/Controllers/V1/FriendRequestController.php
+++ app/Http/Controllers/V1/FriendRequestController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\V1;
 use App\Helpers\Helper;
 use App\Http\Controllers\Controller;
 use App\Models\FriendRequest;
+use App\Models\GroupUsers;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FriendRequestController extends Controller
 {
@@ -24,8 +26,12 @@ class FriendRequestController extends Controller
         ->where('receiver_id', $receiver->id)
         ->first();
 
-        if ($existingRequest) {
+        if ($existingRequest->accepted != 'CANCELLED') {
             return response()->json(['status_code' => 0, 'data' => [], 'message' => 'Friend request already sent']);
+        }
+        if($existingRequest->accepted == 'CANCELLED') {
+            $existingRequest->accepted = 'NO_ACTION';
+            $existingRequest->save();
         }
 
         $friendRequest = new FriendRequest();
@@ -93,12 +99,26 @@ class FriendRequestController extends Controller
         // Retrieve friend details
         $friendDetails = User::whereIn('id', $friendIds)->select('id', 'name', 'email','profile_picture')->get();
 
-        
-        $friendDetails->map(function ($friend) {
+        $loggedInUserGroupIds = [];
+        foreach ($friendIds as $friendId) {
+            $groupId = GroupUsers::whereIn('user_id', [$user->id, $friendId])
+                ->where('groups.type','SINGLE')
+                ->join('groups','groups.id','group_users.group_id')
+                ->groupBy('group_id')
+                ->havingRaw('COUNT(DISTINCT user_id) = 2')
+                ->pluck('group_id')
+                ->first();
+
+            $loggedInUserGroupIds[$friendId] = $groupId;
+        }
+
+        // Map additional data to each friend
+        $friendDetails->map(function ($friend) use ($loggedInUserGroupIds) {
             $friend->type = 'SINGLE';
+            $friend->chatfetchid = $loggedInUserGroupIds[$friend->id] ?? null;
             return $friend;
         });
-
+        
         return response()->json(['status_code' => 1, 'data' => ['friends' => $friendDetails], 'message' => 'Friends fetched successfully']);
     }
     public function fetchFriendRequest(Request $request)
@@ -106,42 +126,55 @@ class FriendRequestController extends Controller
         $user = auth()->user();
     
         // Fetch friend requests involving the user
-        $friendRequests = FriendRequest::where(function ($query) use ($user) {
-            $query->where('sender_id', $user->id)
-                  ->orWhere('receiver_id', $user->id);
-        })
+        // $friendRequests = FriendRequest::where(function ($query) use ($user) {
+        //     $query->where('sender_id', $user->id)
+        //           ->orWhere('receiver_id', $user->id);
+        // })
+        // ->where('accepted', 'NO_ACTION')
+        // ->get();
+        
+        $friendRequests = FriendRequest::where('receiver_id', $user->id)
+        ->join('users', 'users.id', 'friend_requests.sender_id')
+        ->select(
+            'users.id as friend_id',
+            'users.name',
+            DB::raw("CONCAT('" . config('app.media_base_url') . "', users.profile_picture) as profile_picture"),
+            'friend_requests.id as friend_request_id',
+            'friend_requests.accepted as friend_request_status'
+        )
         ->where('accepted', 'NO_ACTION')
         ->get();
-        
+    
+
     
         // Extract friend IDs excluding the user's own ID
-        $friendIds = $friendRequests->pluck('sender_id')->merge($friendRequests->pluck('receiver_id'))->reject(function ($friendId) use ($user) {
-            return $friendId == $user->id;
-        })->unique();
+        // $friendIds = $friendRequests->pluck('sender_id')->merge($friendRequests->pluck('receiver_id'))->reject(function ($friendId) use ($user) {
+        //     return $friendId == $user->id;
+        // })->unique();
 
         // Retrieve friend details and their request status
-        $friendsWithStatus = collect();
-        foreach ($friendIds as $friendId) {
-            $friend = User::find($friendId);
-            $friendRequest = $friendRequests->first(function ($request) use ($friendId, $user) {
-                return ($request->sender_id == $friendId && $request->receiver_id == $user->id) ||
-                       ($request->sender_id == $user->id && $request->receiver_id == $friendId);
-            });
-            $status = $friendRequest ? $friendRequest->accepted : null;
+        // $friendsWithStatus = collect();
+        // foreach ($friendIds as $friendId) {
+        //     $friend = User::find($friendId);
+        //     $friendRequest = $friendRequests->first(function ($request) use ($friendId, $user) {
+        //         return ($request->sender_id == $friendId && $request->receiver_id == $user->id) ||
+        //                ($request->sender_id == $user->id && $request->receiver_id == $friendId);
+        //     });
+        //     $status = $friendRequest ? $friendRequest->accepted : null;
     
-            $friendsWithStatus->push([
-                'friend_request_id' => $friendRequest ? $friendRequest->id : null,
-                'friend_id' => $friend->id,
-                'name' => $friend->name,
-                'email' => $friend->email,
-                'profile_picture' => $friend->profile_picture,
-                'friend_request_status' => $status,
-            ]);
-        }
+        //     $friendsWithStatus->push([
+        //         'friend_request_id' => $friendRequest ? $friendRequest->id : null,
+        //         'friend_id' => $friend->id,
+        //         'name' => $friend->name,
+        //         'email' => $friend->email,
+        //         'profile_picture' => $friend->profile_picture,
+        //         'friend_request_status' => $status,
+         //   ]);
+        // }
     
         return response()->json([
             'status_code' => 1,
-            'data' => ['friends' => $friendsWithStatus],
+            'data' => ['friends' => $friendRequests],
             'message' => 'Friends fetched successfully'
         ]);
     }
